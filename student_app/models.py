8cb7819af6c9d5bd94c8b99ee20dc9bed01b8ced
jack-himlo <devon.g.jackson417@gmail.com>
2025-03-24 22:51:53 -0400

    updated WIP. added locker_reassignment, and student_status methods

--- student_app/models.py
+++ student_app/models.py
@@ -4,8 +4,18 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=100)
     student_email = models.EmailField(unique=True)
-    personal_email = models.EmailField(blank=True, null= True)
-    locker_number = models.IntegerField()
-    locker_combination = models.CharField(max_length=20)
+    personal_email = models.EmailField(blank=True, null= True, unique=True)
+    locker_number = models.IntegerField(default=110, unique= True)
+    locker_combination = models.CharField(max_length=20, default = "12-12-12", blank=True )
     good_student= models.BooleanField(default=True)
 
+    def __str__(self):
+        return f"{self.name} - {self.student_email} - {self.locker_number}"
+    
+    def locker_reassignment(self, new_locker_number):
+        self.locker_number = new_locker_number
+        self.save()
+        return f"Student: {self.student.name}'s locker has been changed to {self.locker_number}"
+    def student_status(self, is_good_student):
+        self.good_student = is_good_student
+        self.save()
